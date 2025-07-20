d8fe4d7a8c38de97d7890daf2f5f57a3a588dcfd
Parham Hatanian <parham.dev@gmail.com>
2025-07-20 13:02:27 +0330

    init project 🎉

---
+++ README.md
@@ -0,0 +1,3 @@
+# gsm_pay_task
+
+A simple Flutter app to demonstrate coding skills for GSM Pay Flutter developer job offer.
---
+++ android/app/src/main/kotlin/ir/gsmpay/gsm_pay_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package ir.gsmpay.gsm_pay_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
