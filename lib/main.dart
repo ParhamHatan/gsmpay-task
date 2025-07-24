830d1c1f15c54fee064916203fe056379df338e2
Parham Hatanian <parham.dev@gmail.com>
2025-07-24 14:18:26 +0330

    - Add toastification wrapper - Lazy put api service - Register themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gsm_pay_task/services/api/api_client_service.dart';
+import 'package:gsm_pay_task/services/api/api_service.dart';
 import 'package:gsm_pay_task/views/routes/pages.dart';
+import 'package:gsm_pay_task/views/theme/app_theme.dart';
+import 'package:toastification/toastification.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Get.lazyPut(() => ApiService(client: ApiClientService()));
+
   runApp(const MainApp());
 }
 
@@ -11,11 +19,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'GSM Pay Task',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      getPages: Pages.pages,
+    return ToastificationWrapper(
+      child: GetMaterialApp(
+        title: 'GSM Pay Task',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        textDirection: TextDirection.rtl,
+        theme: AppTheme.lightTheme(),
+        themeMode: ThemeMode.light,
+        getPages: Pages.pages,
+      ),
     );
   }
 }
