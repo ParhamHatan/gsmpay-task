213c425d561e2b1bbad1a60a491f3633634325bb
Parham Hatanian <parham.dev@gmail.com>
2025-07-24 14:17:30 +0330

    Linting

--- lib/views/routes/pages.dart
+++ lib/views/routes/pages.dart
@@ -5,7 +5,7 @@ import 'package:gsm_pay_task/views/routes/routes.dart';
 import 'package:gsm_pay_task/views/screens/home_form_screen.dart';
 
 abstract class Pages {
-  static List<GetPage<dynamic>> pages = [
+  static final pages = [
     GetPage<dynamic>(
       name: Routes.homeForm,
       page: () => const HomeFormScreen(),
